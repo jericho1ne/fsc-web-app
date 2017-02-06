e2f50174242bd12fbee31285c7f32a398b3404c3
Mihai Peteu <jericho1ne@yahoo.com>
2017-02-05 23:44:32 -0800

    Vue-ing it up!

---
+++ src/components/Hello.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ msg2 }}</h2>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <br>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      msg2: 'Subheader'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
